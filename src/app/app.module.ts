85f14b809d6740da56164207640d78d9180f32e5
Konstantin Slavin <konstantin@kultprosvet.net>
2018-05-23 17:24:48 +0300

    code refactor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,24 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-// Store
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import 'hammerjs';
 
 import { AppComponent } from './app.component';
 import { NotFoundComponent } from './shared/components/not-found/not-found.component';
 import { AppRoutingModule } from './app-routing.module';
 import { SystemModule } from './system/system.module';
-
+import { reducers } from '@state';
+import { WeatherEffects } from '@actions/weather.effects';
 import { environment } from '@env/environment';
-
-import { reducers } from './store/app.reducers';
-import { WeatherEffects } from './system/store/weather.effects';
-
 import { SharedInterceptor } from './shared/interceptor/shared.interceptor';
 
-import 'hammerjs';
 
 @NgModule({
 	declarations: [
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'http://api.openweathermap.org/data/2.5/forecast',
+	apiIconUrl: 'http://openweathermap.org/img/w',
+  appKey: '747a52cf5f5f77c4b00674267fc0df6b',
 };
+
